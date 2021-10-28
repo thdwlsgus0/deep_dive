bd58a4f2dfb36817ee0fa486cd9e2891b0d316dc
thdwlsgus0 <thdwlsgus0@nts-corp.com>
2021-10-28 16:26:33 +0900

    딥다이브 학습

---
+++ 29장/29-11.js
@@ -0,0 +1,2 @@
+console.log(Math.max.apply(null, [1, 2, 3]));
+console.log(Math.max(...[1, 2, 3]));
---
+++ 29장/29-2.js
@@ -0,0 +1,6 @@
+console.log(Math.abs(-1));
+console.log(Math.abs('-1'));
+console.log(Math.abs(''));
+console.log(Math.abs([]));
+console.log(Math.abs(null));
+console.log(Math.abs(undefined));
